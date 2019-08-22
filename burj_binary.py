b8a1d27b1969a78dfd1bb650068a65157a022a4d
Abhishek Dubey <45308951+dubeyji10@users.noreply.github.com>
2019-08-22 17:12:58 +0530

    Update burj_binary.py

--- burj_binary.py
+++ burj_binary.py
@@ -1,8 +1,18 @@
-n = int(input('enter the number '))
-h = len(str(bin(n))[2:])
+#n = int(input('enter the number '))
+#h = len(str(bin(n))[2:])
 
-for k in range(n+1):
-    tt=str(bin(k))[2:]
-    p=len(tt)
-    print(' '*(h-p)+tt)
+#for k in range(n+1):
+#    tt=str(bin(k))[2:]
+#    p=len(tt)
+#    print(' '*(h-p)+tt)
+# or we could do this  -___________________
 
+
+
+n = eval(input(""))
+if n>15:
+    for i in range(0,n+1):
+        print("{:5b}".format(i))
+else:
+    for i in range(0,n+1):
+        print("{:4b}".format(i))
