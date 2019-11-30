f1957d6747ff1854e9224fdfa9cff84dc90712b0
Abhishek Dubey <45308951+dubeyji10@users.noreply.github.com>
2019-11-30 22:50:29 +0530

    Update even_odd_without_mod.py

--- even_odd_without_mod.py
+++ even_odd_without_mod.py
@@ -1,8 +1,5 @@
 def even_odd(n):
-    ise = 'Even'
-    for i in range(1, n+1):
-        ise = 'Odd' if ise == 'Even' else 'Even'
-    return ise
+    return 'odd' if n&1 else 'even'
 
 
 a = int(input('enter the number '))
